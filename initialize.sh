bdcfe2b68aed2adefbcae24665c403a453dc7d22
Evan Hoge <evanhoge123@gmail.com>
2025-06-26 01:25:18 -0400

    fixed init script

--- initialize.sh
+++ initialize.sh
@@ -1,2 +1,2 @@
 mkdir build
-cd cookies && python3 -m venv .venv && ./.venv/bin/pip3 install browser_cookies3 && ./collect.sh
+cd cookies && python3 -m venv .venv && ./.venv/bin/pip3 install browser_cookie3 && ./collect.sh
